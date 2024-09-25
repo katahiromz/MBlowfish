4a59d046b190c155b4eb6e73423a018c55cfac89
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-09-25 14:16:32 +0900

    first commit

---
+++ MBlowfish.h
@@ -0,0 +1,35 @@
+// MBlowfish.h -- Blowfish encrypter and decrypter
+////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <cstdlib>
+#include <cstdint>
+
+struct MBlowfishImpl;
+
+class MBlowfish
+{
+public:
+    MBlowfish();
+    MBlowfish(const char *passwd);
+    MBlowfish(const char *passwd, size_t len);
+    virtual ~MBlowfish();
+
+    void SetPassword(const char *passwd);
+    void SetPassword(const char *passwd, size_t len);
+
+    // NOTE: Parameter bytes must be a multiple of 8.
+    bool Encrypt(void *ptr, uint32_t bytes);
+    bool Decrypt(void *ptr, uint32_t bytes);
+
+    uint8_t *EncryptWithLength(const void *ptr, uint32_t& length);
+    uint8_t *DecryptWithLength(const void *ptr, uint32_t& length);
+
+    void Reset();
+
+protected:
+    MBlowfishImpl *m_pimpl;
+};
+
+////////////////////////////////////////////////////////////////////////////
---
+++ README.md
@@ -0,0 +1,5 @@
+# MBlowfish by katahiromz
+
+MBlowfish is Blowfish encrypter and decrypter.
+
+Written by Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>.
